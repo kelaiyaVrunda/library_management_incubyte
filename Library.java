af2128208a16a770bd3a03920b827fd674dd6476
kelaiyaVrunda <165145147+kelaiyaVrunda@users.noreply.github.com>
2024-08-31 19:07:11 +0530

    add book function in library

--- Library.java
+++ Library.java
@@ -1,3 +1,13 @@
-public class Library {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Library<books> {
+    private Map<String, Book> books;
+
+    public Library() {
+        books = new HashMap<>();
+    }
+
+    public void addBook(Book book) {
+    }
 }
