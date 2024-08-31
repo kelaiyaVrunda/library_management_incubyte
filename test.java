3aee8dfe85fdf672c4366ae3d4e47aea3d140be5
kelaiyaVrunda <165145147+kelaiyaVrunda@users.noreply.github.com>
2024-08-31 20:18:37 +0530

    borrow function added to test

--- test.java
+++ test.java
@@ -5,8 +5,13 @@ public class test {
     @Test
     public void testAddBook() {
         Library library = new Library();
-        Book book = new Book("1234", "Effective java", "Joshua Bloch",2018);
+        Book book = new Book("1234", "Effective java", "Joshua Bloch", 2018);
         library.addBook(book);
-
     }
+    @Test
+    public void testBorrowBook() {
+    Library library = new Library();
+    library.borrowBook("1234");
+    }
+
 }
